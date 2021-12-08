ebb898f580f6be0ac6965ca8a00147bfcbbc3eb8
Jason Shuler <jshuler@gmail.com>
2021-12-07 20:25:27 -0500

    GM: Enable forwarding for camera-harnesses

--- board/safety/safety_gm.h
+++ board/safety/safety_gm.h
@@ -34,6 +34,12 @@ AddrCheckStruct gm_addr_checks[] = {
 #define GM_RX_CHECK_LEN (sizeof(gm_addr_checks) / sizeof(gm_addr_checks[0]))
 addr_checks gm_rx_checks = {gm_addr_checks, GM_RX_CHECK_LEN};
 
+int gm_good_cam_cnt = 0; // Counter for good lkas messages seen on cam bus
+bool gm_allow_fwd = false; // Enables forwarding (unless blocked)
+bool gm_block_fwd = false; // Permanently disables forwarding
+int gm_camera_bus = 2; // (default) Camera harness uses bus 2. OBD harness uses 1.
+//bool gm_has_relay = true; // Camera harness has a relay (future use)
+
 static int gm_rx_hook(CANPacket_t *to_push) {
 
   bool valid = addr_safety_check(to_push, &gm_rx_checks, NULL, NULL, NULL);
@@ -204,8 +210,79 @@ static int gm_tx_hook(CANPacket_t *to_send) {
   return tx;
 }
 
+static int gm_fwd_hook(int bus_num, CANPacket_t *to_fwd) {
+
+  int bus_fwd = -1;
+
+  if (gm_allow_fwd && !gm_block_fwd) {
+    if (bus_num == 0) {
+      // Message 388 causes the stock camera to display hands-off warnings if the driver has left LKAS enabled
+      // Note that the LKAS switch is hard-wired to the camera - we cannot turn it off programmatically
+      // Blocking 388 causes dash errors if the driver presses the lkas button
+      // Possible future plan: Catch message 388 and edit the HandsOffSWlDetectionStatus to 1 (0 is hands off)
+      // when forwarding to camera
+      bus_fwd = gm_camera_bus;
+    }
+    else if (bus_num == gm_camera_bus) {
+      int addr = GET_ADDR(to_fwd);
+      // block stock lkas messages and stock acc messages (if OP is doing ACC)
+      // For now, if the car supports cam-based ACC, it is passed through
+      // TODO: Make ACC blocking optional (Allow user to select between OP and stack VOACC)
+      int is_lkas_msg = (addr == 384);
+      int is_acc_msg = false;
+      //int is_acc_msg = (addr == 0x343);
+      int block_msg = (is_lkas_msg || is_acc_msg);
+      if (!block_msg) {
+        bus_fwd = 0;
+      }
+    }
+  }
+  else {
+    // Evaluate traffic to determine if forwarding should be enabled (only camera is on camera bus)
+    // The designated cam bus is sometimes connected to the chassis or object bus
+    // Forwarding should only be enabled after confirming there is a camera where it should be
+    // Once 10 good frames (and no obvious bad ones) are seen, we latch forwarding on
+    if (!gm_allow_fwd && !gm_block_fwd && (bus_num == gm_camera_bus)) {
+      int addr = GET_ADDR(to_fwd);
+      int len = GET_LEN(to_fwd);
+      // Check for a list of well-known messages that will never be present on a camera and block forwarding permanently
+      if (((addr == 384) && (len != 4)) //chassis bus has 384 of different size
+        || (addr == 1120) // F_LRR_Obj_Header from object bus
+        || (addr == 784) // ASCMHeadlight from object bus
+        || (addr == 309) // LHT_CameraObjConfirmation_FO from object bus
+        || (addr == 192) // Unknown id only on chassis bus
+      ) {
+        gm_block_fwd = true;
+      }
+      else {
+        gm_good_cam_cnt++;
+      }
+      // Wait for 10 LKAS frames of the correct size before enabling forwarding
+      if (gm_good_cam_cnt > 10) {
+        gm_allow_fwd = true;
+      } 
+    }
+  }
+
+  return bus_fwd;
+}
+
 static const addr_checks* gm_init(int16_t param) {
   UNUSED(param);
+
+  gm_good_cam_cnt = 0;
+  gm_allow_fwd = false;
+  gm_block_fwd = false;
+  gm_camera_bus = 2;
+  //gm_has_relay = true;
+
+  if (car_harness_status == HARNESS_STATUS_NC) {
+    //OBD harness and older pandas use bus 1 and no relay
+    //gm_has_relay = false;
+    gm_camera_bus = 1;
+  }
+
+
   controls_allowed = false;
   relay_malfunction_reset();
   return &gm_rx_checks;
@@ -216,5 +293,5 @@ const safety_hooks gm_hooks = {
   .rx = gm_rx_hook,
   .tx = gm_tx_hook,
   .tx_lin = nooutput_tx_lin_hook,
-  .fwd = default_fwd_hook,
+  .fwd = gm_fwd_hook,
 };
